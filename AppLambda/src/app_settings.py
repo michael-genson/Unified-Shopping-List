1ff8caa37ef9428a5108a0bf0e840bf99a3edfbf
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2023-04-18 04:47:49 +0000

    Bump version: 0.3.6 → 0.3.7

--- AppLambda/src/app_settings.py
+++ AppLambda/src/app_settings.py
@@ -34,7 +34,7 @@ class AppSecrets(BaseSettings):
 class AppSettings(BaseSettings):
     ### About ###
     app_title = "Unified Shopping List"
-    app_version = "0.3.6"
+    app_version = "0.3.7"
     internal_app_name = "shopping_list_api"
 
     ### App ###
